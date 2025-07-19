f3008380adce778d76b3f86eac2f2b823371fe89
ReClaim <dustin.stormnation@gmail.com>
2025-07-19 03:09:35 -0500

    Add files via upload

---
+++ roofing-inspection-app.tsx
@@ -0,0 +1,13 @@
+// roofing-inspection-app.tsx
+import React, { useState, useRef } from 'react';
+
+const InspectionTool = () => {
+  return (
+    <div>
+      <h1>Reclaim Inspection Tool</h1>
+      {/* Component code here */}
+    </div>
+  );
+};
+
+export default InspectionTool;
